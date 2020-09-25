759965dec087532d79f2f7e8392d834eb683ff0e
cc <chengguangxin927@gmail.com>
2020-09-25 17:38:34 +0800

    工厂方法模式测试新增反射模式

--- TestFactoryMethod.php
+++ TestFactoryMethod.php
@@ -4,8 +4,25 @@ use Creational\FactoryMethod\DatabaseLoggerFactory;
 
 require 'vendor/autoload.php';
 
-$factory = new DatabaseLoggerFactory();
+// 通过反射
+
+//配置文件可以放在单独的文件中
+$config = [
+    'LoggerFactory' => DatabaseLoggerFactory::class
+];
+
+$class = new ReflectionClass($config['LoggerFactory']);
+
+$factory = $class->newInstance();
 
 $logger = $factory->createLogger();
 
 $logger->writeLog();
+
+// 普通模式
+
+// $factory = new Creational\FactoryMethod\DatabaseLoggerFactory();
+
+// $logger = $factory->createLogger();
+
+// $logger->writeLog();
