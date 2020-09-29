931d7b5d8d5c1b72ebb896aec0805e60d9ab1dab
cc <chengguangxin927@gmail.com>
2020-09-29 18:07:55 +0800

    新增 原型模式模式

---
+++ Creational/Prototype/AbstractPrototype.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace Creational\Prototype;
+
+
+
+Abstract class AbstractPrototype
+{
+    abstract public function copy();
+}
---
+++ Creational/Prototype/Attachment.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Creational\Prototype;
+
+
+class Attachment
+{
+    private $name;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function download()
+    {
+        echo '下载附件，文件名为'.$this->name.PHP_EOL;
+    }
+}
